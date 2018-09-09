45261ca97cf032702177c1ac5654c416189662e0
Malin Lorch <ml224cv@student.lnu.se>
2018-09-09 12:23:56 -0600

    DateTimeView returning timestamp and date

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,12 +1,27 @@
 <?php
 
-class DateTimeView {
+	class DateTimeView {
+		private $year;
+		private $month;
+		private $monthDay;
+		private $weekDay;
+		private $timeStamp;
 
+		public function show() {
+		$dateArray = getDate();
+		
+		$year = $dateArray["year"];
+		$month = $dateArray["month"]; 
+		$monthDay = $dateArray["mday"];
+		$weekDay = $dateArray["weekday"];
+		$hours = $dateArray["hours"];
+		$minutes = $dateArray["minutes"];
+		$seconds = $dateArray["seconds"];
 
-	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = "$weekDay, the $monthDay of $month $year. The time is $hours:$minutes:$seconds";
 
 		return '<p>' . $timeString . '</p>';
 	}
+
+
 }
